a37b278b726bd7a082678776bbe6321c68204dc7
Peter Souter <peter.souter@puppetlabs.com>
2015-01-23 16:20:52 +0000

    Implements tags for RDS
    This is a lot harder than the other types, as they all have a #tags helper to get tags. With RDS, you have to use the `#add_tags_to_resource` and `#list_tags_for_resource` methods

--- lib/puppet/provider/rds_instance/v2.rb
+++ lib/puppet/provider/rds_instance/v2.rb
@@ -35,10 +35,8 @@ Puppet::Type.type(:rds_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aw
   end
 
   def self.db_instance_to_hash(region, instance)
-    #tags = {}
-    #instance.tags.each do |tag|
-    #  tags[tag.key] = tag.value unless tag.key == 'Name'
-    #end
+    tags = rds_client(region).list_tags_for_resource(resource_name: get_arn_for_instance(region,instance.db_instance_identifier))
+
     config = {
       ensure: :present,
       name: instance.db_instance_identifier,
@@ -52,6 +50,7 @@ Puppet::Type.type(:rds_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aw
       license_model: instance.license_model,
       multi_az: instance.multi_az,
       iops: instance.iops,
+      tags: tags.tag_list.map { |tag| {key: tag.key, value: tag.value} }
     }
     config
   end
@@ -89,25 +88,28 @@ Puppet::Type.type(:rds_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aw
     response = rds_client(resource[:region]).create_db_instance(config)
 
     @property_hash[:ensure] = :present
-    #tags = resource[:tags] ? resource[:tags].map { |k,v| {key: k, value: v} } : []
-    #tags << {key: 'Name', value: name}
-    #rds_client(resource[:region]).create_tags(
-    #  resources: response.instances.map(&:instance_id),
-    #  tags: tags)
+    tags = resource[:tags] ? resource[:tags].map { |k,v| {key: k, value: v} } : []
+    tags << {key: 'Name', value: name}
+    response.each do |page|
+      Puppet.info("Adding tags to #{page}")
+    end
+    rds_client(resource[:region]).add_tags_to_resource(
+     resource_name: get_arn_for_instance(region,resource[:name]),
+     tags: tags)
   end
 
-#  def tags=(value)
-#    Puppet.info("Updating tags for #{name} in region #{region}")
-#    ec2_client(resource[:region]).create_tags(
-#      resources: [instance_id],
-#      tags: value.collect { |k,v| { :key => k, :value => v } }
-#    ) unless value.empty?
-#    missing_tags = tags.keys - value.keys
-#    ec2_client(resource[:region]).delete_tags(
-#      resources: [instance_id],
-#      tags: missing_tags.collect { |k| { :key => k } }
-#    ) unless missing_tags.empty?
-#  end
+ def tags=(value)
+   Puppet.info("Updating tags for #{name} in region #{region}")
+   rds_client(resource[:region]).add_tags_to_resource(
+     resource_name: get_arn_for_instance(region,resource[:name]),
+     tags: value.collect { |k,v| { :key => k, :value => v } }
+   ) unless value.empty?
+   missing_tags = tags.keys - value.keys
+   rds_client(resource[:region]).delete_tags(
+     resource_name: get_arn_for_instance(region,resource[:name]),
+     tags: missing_tags.collect { |k| { :key => k } }
+   ) unless missing_tags.empty?
+ end
 
   def destroy
     Puppet.info("Deleting instance #{name} in region #{resource[:region]}")
